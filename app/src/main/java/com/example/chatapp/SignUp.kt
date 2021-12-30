a2205943b4ad175ac8488d2c5d5775ca5dd3bd35
Aryan61905 <96496036+Aryan61905@users.noreply.github.com>
2021-12-29 22:14:30 -0500

    Creating SignUp method

--- app/src/main/java/com/example/chatapp/SignUp.kt
+++ app/src/main/java/com/example/chatapp/SignUp.kt
@@ -1,9 +1,11 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
@@ -36,6 +38,14 @@ class SignUp : AppCompatActivity() {
     }
 
     private fun signUp(email: String, password: String){
-        
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@SignUp, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@SignUp, "ERROR 05", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
